03af8d31bd0bd73fba854d05825d8156dd59e8d9
smgD371 <nikolay.drenchev@gmail.com>
2023-11-17 16:51:18 +0200

    new

--- src/frame/SignUpFrame.java
+++ src/frame/SignUpFrame.java
@@ -1,5 +1,40 @@
 package frame;
 
-public class SignUpFrame {
+import javax.swing.*;
+
+public class SignUpFrame extends JFrame {
+
+    private JButton signUpButton;
+    private JLabel usernameLbl;
+    private JLabel passwordLbl;
+    private JLabel register;
+    private JTextField usernameTxF;
+    private JPasswordField passwordFieldPwdF;
+
+    SignUpFrame () {
+        this.setTitle ("Banking app");
+        this.setSize(400,300);
+        this.setLayout(null);
+        usernameLbl = new JLabel("Username:");
+        usernameTxF = new JTextField();
+        passwordLbl = new JLabel("Password:");
+        passwordFieldPwdF = new JPasswordField();
+        signUpButton = new JButton("Sign up");
+
+        usernameLbl.setBounds(40,20,100,20);
+        usernameTxF.setBounds(140,20,100,20);
+        passwordLbl.setBounds(40,60,100,20);
+        passwordFieldPwdF.setBounds(140,60,100,20);
+        signUpButton.setBounds(180,160,180,20);
+
+        this.add(usernameLbl);
+        this.add(usernameTxF);
+        this.add(passwordLbl);
+        this.add(passwordFieldPwdF);
+        this.add(signUpButton);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
 
 }
